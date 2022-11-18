1ebbcf7beb2b1f38932f275a7b5a71d01fef10b0
Savita Rankavat <savita@Savitas-MacBook-Pro.local>
2022-11-17 21:25:58 -0600

    added javascript file

---
+++ script.js
@@ -0,0 +1,45 @@
+var stats = {
+    wins: 0,
+    losses: 0,
+    ties: 0
+}
+
+var gameOptions = ["R", "P", "S"];
+
+var rockPaperscissors = function() {
+    var userChoice = window.prompt("Please enter R, S or P").toUpperCase();
+    console.log(userChoice);
+
+
+    var index = Math.floor(Math.random() * gameOptions.length); 
+
+    var computerChoice = gameOptions[index];
+    console.log(computerChoice);
+
+    if (userChoice === computerChoice) {
+     window.confirm("Tied! Try Again");
+     stats.ties++;
+     return rockPaperscissors();
+    } else if ((userChoice === "R" && computerChoice === "S") ||
+    (userChoice === "P" && computerChoice === "R") ||
+    (userChoice === "S" && computerChoice === "P")) {
+        window.confirm("Congrats! You won.")
+        stats.wins++;
+    } else {
+        window.confirm("Sorry! You lose.")
+        stats.losses++;
+    }
+
+   var keepPlaying = window.prompt("Do you want to continue playing?").toLowerCase();
+
+   
+   if (keepPlaying === "yes") {
+    rockPaperscissors();
+   } else {
+    window.confirm("Wins" + "=" + stats.wins.toString() + "," + " Losses" + "=" + stats.losses.toString() + "," +
+    "Ties" + "=" + stats.ties.toString());
+   }
+
+}
+
+rockPaperscissors();
